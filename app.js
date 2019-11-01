cd3ba64d4e17f1546091e7480b18fac971d60576
cit-team <mt.fujiwa9wa9@gmail.com>
2019-11-01 16:31:49 +0900

    tmp

---
+++ app.js
@@ -0,0 +1,18 @@
+/* 1. expressモジュールをロードし、インスタンス化してappに代入。*/
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var url = require('url');
+
+/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
+var server = app.listen(3000, function () {
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
+app.use(express.static('public'));
+app.use(bodyParser.json());
+// app.get('/', function (req, res) {
+//     res.send('Hello World!!!!!!!!!!');
+// });
+app.post('/enterroom', function (req, res) {
+    res.send(main.html);
+});
