278caf93bbe07d49beca533dc058e24030689435
cit-team <mt.fujiwa9wa9@gmail.com>
2019-11-01 14:25:32 +0900

    FIX: 生成されるHTMLの中で変数を使うようにした & underfindを削除した.

--- src/js/displayTable.js
+++ src/js/displayTable.js
@@ -14,16 +14,18 @@ function displayTable(){
 }
 
 function generateRow(num,indexes){
-	var generatedRow;
+	var generatedRow="";
 	for(i=0;i<num;i++){
 		generatedRow += '<td>';
 		for(j=0;j<stoneAddress[indexes[i]];j++){
 			console.log("stoneAddress[indexes]"+stoneAddress[indexes[i]]);
-			console.log("indexesの数"+ indexes);
-			generatedRow += '<input type="image" src="img/065160.png" alt="stone" `value=\"${indexes[j]}\"` width="50" height="50" `onclick=\"${move(value)}\"`>';
+			console.log("indexesの数"+ indexes[i]);
+			let val = String(indexes[i]);
+			generatedRow += `<input type=image src=img/GreenStone.png alt=stone value=${val} width=50 height=50 onclick=move(value)>`;
 		}
 		generatedRow += '</td>';
 	}
+	console.log("generatedRowの表示"+generatedRow);
 	return generatedRow;
 }
 
